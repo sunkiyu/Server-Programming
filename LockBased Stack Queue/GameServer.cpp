d8561388022c638c259d930303e063dd118179df
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-01-24 22:19:50 +0900

    Create GameServer.cpp

---
+++ LockBased Stack Queue/GameServer.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <future>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
+
+LockQueue<int32> q;
+LockStack<int32> s;
+
+void Push()
+{
+	while (1)
+	{
+		int32 value = rand() % 10;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
+	}
+}
+
+void Pop()
+{
+	while (1)
+	{
+		
+		int32 data;
+		//waitPop도 가능
+		if (q.TryPop(data))
+		{
+			cout << data << endl;
+		}
+
+	}
+}
+
+int main()
+{
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
+
+	t1.join();
+	t2.join();
+	t3.join();
+}
