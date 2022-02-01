0d84d723f2d443c3e5b8c0f6c1de71d99dd0434a
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-02-01 20:44:28 +0900

    Add files via upload

---
+++ Reader-Writer-Lock/GameServer/GameServer.cpp
@@ -0,0 +1,79 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+class TestLock
+{
+	USE_LOCK;
+
+public:
+	int32 TestRead()
+	{
+		READ_LOCK;
+
+		if (_queue.empty())
+			return -1;
+
+		return _queue.front();
+	}
+
+	void TestPush()
+	{
+		WRITE_LOCK;
+
+		_queue.push(rand() % 100);
+	}
+
+	void TestPop()
+	{
+		WRITE_LOCK;
+		
+		if (_queue.empty() == false)
+			_queue.pop();
+	}
+
+private:
+	queue<int32> _queue;
+};
+
+TestLock testLock;
+
+void ThreadWrite()
+{
+	while (true)
+	{
+		testLock.TestPush();
+		this_thread::sleep_for(1ms);
+		testLock.TestPop();
+	}
+}
+
+void ThreadRead()
+{
+	while (true)
+	{
+		int32 value = testLock.TestRead();
+		cout << value << endl;
+		this_thread::sleep_for(1ms);
+	}
+}
+
+int main()
+{
+	for (int32 i = 0; i < 2; i++)
+	{
+		GThreadManager->Launch(ThreadWrite);
+	}
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadRead);
+	}
+
+	GThreadManager->Join();
+}
---
+++ Reader-Writer-Lock/ServerCore/CoreGlobal.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CoreGlobal.h"
+#include "ThreadManager.h"
+
+ThreadManager* GThreadManager = nullptr;
+
+class CoreGlobal
+{
+public:
+	CoreGlobal()
+	{
+		GThreadManager = new ThreadManager();
+	}
+
+	~CoreGlobal()
+	{
+		delete GThreadManager;
+	}
+} GCoreGlobal;
