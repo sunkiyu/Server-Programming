53c3b6f38c6cea487cb662414dc60bcf7887fb88
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-02-08 22:18:22 +0900

    Add files via upload

---
+++ Memory Pool#1/GameServer/GameServer.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+#include "RefCounting.h"
+#include "Memory.h"
+#include "Allocator.h"
+
+class Player
+{
+public:
+	Player() {}
+	virtual ~Player() {}
+};
+
+class Knight : public Player
+{
+public:
+	Knight()
+	{
+		cout << "Knight()" << endl;
+	}
+
+	Knight(int32 hp) : _hp(hp)
+	{
+		cout << "Knight(hp)" << endl;
+	}
+
+	~Knight()
+	{
+		cout << "~Knight()" << endl;
+	}
+
+	int32 _hp = 100;
+	int32 _mp = 10;
+};
+
+int main()
+{
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					Vector<Knight> v(10);
+
+					//Map 트리 노드 사용
+					Map<int32, Knight> m;
+					m[100] = Knight();
+
+					this_thread::sleep_for(10ms);
+				}
+			});
+	}
+
+	GThreadManager->Join();
+}
