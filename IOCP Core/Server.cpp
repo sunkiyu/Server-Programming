587593bbc9ed575c38fed822c1a00ff9f113703f
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-01 16:04:43 +0900

    Create Server.cpp

---
+++ IOCP Core/Server.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+
+#include "SocketUtils.h"
+#include "Listener.h"
+
+int main()
+{
+	Listener listener;
+	listener.StartAccept(NetAddress(L"127.0.0.1", 7777));
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([=]()
+			{
+				while (true)
+				{
+					GIocpCore.Dispatch();
+				}				
+			});
+	}	
+
+	GThreadManager->Join();
+}
