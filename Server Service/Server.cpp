ac8f50870dc5f669f721d7e96cdf52e076a358db
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-02 21:28:48 +0900

    Create Server.cpp

---
+++ Server Service/Server.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
+
+#include "Service.h"
+#include "Session.h"
+
+class GameSession : public Session
+{
+
+};
+
+
+int main()
+{
+	ServerServiceRef service = MakeShared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		MakeShared<IocpCore>(),
+		MakeShared<GameSession>, // TODO : SessionManager 등
+		100);
+
+	ASSERT_CRASH(service->Start());
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([=]()
+			{
+				while (true)
+				{
+					service->GetIocpCore()->Dispatch();
+				}				
+			});
+	}	
+
+	GThreadManager->Join();
+}
