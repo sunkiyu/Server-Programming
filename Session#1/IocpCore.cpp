6c4fc130d678f1bccc4456d82c4d2ac4209ab8da
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-03 21:18:18 +0900

    Create IocpCore.cpp

---
+++ Session#1/IocpCore.cpp
@@ -0,0 +1,56 @@
+#include "pch.h"
+#include "IocpCore.h"
+#include "IocpEvent.h"
+
+/*--------------
+	IocpCore
+---------------*/
+
+IocpCore::IocpCore()
+{
+	_iocpHandle = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+	ASSERT_CRASH(_iocpHandle != INVALID_HANDLE_VALUE);
+}
+
+IocpCore::~IocpCore()
+{
+	::CloseHandle(_iocpHandle);
+}
+
+bool IocpCore::Register(IocpObjectRef iocpObject)
+{
+	//iocpObject 키값으로 사용하지 않는다.
+	return ::CreateIoCompletionPort(iocpObject->GetHandle(), _iocpHandle, /*key*/0, 0);
+}
+
+bool IocpCore::Dispatch(uint32 timeoutMs)
+{
+	DWORD numOfBytes = 0;
+	//CreateIoCompletionPort에서 0으로 넣었기 떄문에 딱히 사용 안함.
+	ULONG_PTR key = 0;	
+	IocpEvent* iocpEvent = nullptr;
+
+	if (::GetQueuedCompletionStatus(_iocpHandle, OUT &numOfBytes, OUT &key, OUT reinterpret_cast<LPOVERLAPPED*>(&iocpEvent), timeoutMs))
+	{
+		//너를 물고있는 owner가 누구냐?
+		IocpObjectRef iocpObject = iocpEvent->owner;
+		//iocpObject가 아직까지 살아 있을까? 레퍼런스 카운팅 RefCountable 상속받는다.
+		iocpObject->Dispatch(iocpEvent, numOfBytes);
+	}
+	else
+	{
+		int32 errCode = ::WSAGetLastError();
+		switch (errCode)
+		{
+		case WAIT_TIMEOUT:
+			return false;
+		default:
+			// TODO : 로그 찍기
+			IocpObjectRef iocpObject = iocpEvent->owner;
+			iocpObject->Dispatch(iocpEvent, numOfBytes);
+			break;
+		}
+	}
+
+	return true;
+}
