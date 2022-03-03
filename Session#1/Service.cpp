c4ee7b1f15529d9e2c6a268b2658a9a2f5ef3737
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-03 21:16:46 +0900

    Create Service.cpp

---
+++ Session#1/Service.cpp
@@ -0,0 +1,93 @@
+#include "pch.h"
+#include "Service.h"
+#include "Session.h"
+#include "Listener.h"
+
+/*-------------
+	Service
+--------------*/
+
+Service::Service(ServiceType type, NetAddress address, IocpCoreRef core, SessionFactory factory, int32 maxSessionCount)
+	: _type(type), _netAddress(address), _iocpCore(core), _sessionFactory(factory), _maxSessionCount(maxSessionCount)
+{
+
+}
+
+Service::~Service()
+{
+}
+
+void Service::CloseService()
+{
+	// TODO
+}
+
+SessionRef Service::CreateSession()
+{
+	SessionRef session = _sessionFactory();
+	//세션이 서비스를 들고 있을 수 있게 한다.
+	session->SetService(shared_from_this());
+
+	if (_iocpCore->Register(session) == false)
+		return nullptr;
+
+	return session;
+}
+
+void Service::AddSession(SessionRef session)
+{
+	WRITE_LOCK;
+	_sessionCount++;
+	_sessions.insert(session);
+}
+
+void Service::ReleaseSession(SessionRef session)
+{
+	WRITE_LOCK;
+	//_sessions가 하나라도 물고 있는 순간 RefCount 가 최소 1이기 때문에 삭제되지 않는다.
+	ASSERT_CRASH(_sessions.erase(session) != 0);
+	_sessionCount--;
+}
+
+/*-----------------
+	ClientService
+------------------*/
+
+ClientService::ClientService(NetAddress targetAddress, IocpCoreRef core, SessionFactory factory, int32 maxSessionCount)
+	: Service(ServiceType::Client, targetAddress, core, factory, maxSessionCount)
+{
+}
+
+bool ClientService::Start()
+{
+	// TODO
+	return true;
+}
+
+ServerService::ServerService(NetAddress address, IocpCoreRef core, SessionFactory factory, int32 maxSessionCount)
+	: Service(ServiceType::Server, address, core, factory, maxSessionCount)
+{
+}
+
+bool ServerService::Start()
+{
+	if (CanStart() == false)
+		return false;
+
+	_listener = MakeShared<Listener>();
+	if (_listener == nullptr)
+		return false;
+
+	ServerServiceRef service = static_pointer_cast<ServerService>(shared_from_this());
+	if (_listener->StartAccept(service) == false)
+		return false;
+
+	return true;
+}
+
+void ServerService::CloseService()
+{
+	// TODO
+
+	Service::CloseService();
+}
