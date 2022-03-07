07c3cf7ea19871e50e26e192f816ef0afc3e9223
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-07 21:47:43 +0900

    Create Server.cpp

---
+++ Session#3/Server.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "ThreadManager.h"
+#include "Service.h"
+#include "Session.h"
+
+class GameSession : public Session
+{
+public:
+	~GameSession()
+	{
+		cout << "~GameSession" << endl;
+	}
+
+	virtual int32 OnRecv(BYTE* buffer, int32 len) override
+	{
+		// Echo
+		cout << "OnRecv Len = " << len << endl;
+		Send(buffer, len);
+		return len;
+	}
+
+	virtual void OnSend(int32 len) override
+	{ 
+		cout << "OnSend Len = " << len << endl;
+	}
+};
+
+int main()
+{
+	ServerServiceRef service = MakeShared<ServerService>(
+		NetAddress(L"127.0.0.1", 7777),
+		MakeShared<IocpCore>(),
+		MakeShared<GameSession>, // TODO : SessionManager 등
+		100);
+
+	ASSERT_CRASH(service->Start());
+
+	for (int32 i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch([=]()
+			{
+				while (true)
+				{
+					service->GetIocpCore()->Dispatch();
+				}				
+			});
+	}	
+
+	GThreadManager->Join();
+}
