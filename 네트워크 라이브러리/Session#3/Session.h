18c435aeb4c03114b44ef9056b7d76e4f078d844
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-10 17:18:12 +0900

    Rename Session#3/Session.h to 네트워크 라이브러리/Session#3/Session.h

--- 네트워크 라이브러리/Session#3/Session.h
+++ 네트워크 라이브러리/Session#3/Session.h
@@ -20,9 +20,9 @@ public:
 	virtual ~Session();
 
 public:
-						/* ¿ÜºÎ¿¡¼­ »ç¿ë */
+						/* ì™¸ë¶€ì—ì„œ ì‚¬ìš© */
 	void				Send(BYTE* buffer, int32 len);
-	//¼­¹ö³¢¸® ¿¬°áÇÒ ÀÏÀÌ ÀÖ±â¶§¹®¿¡ Connect ÇÑ´Ù.
+	//ì„œë²„ë¼ë¦¬ ì—°ê²°í•  ì¼ì´ ìˆê¸°ë•Œë¬¸ì— Connect í•œë‹¤.
 	bool				Connect();
 	void				Disconnect(const WCHAR* cause);
 
@@ -30,7 +30,7 @@ public:
 	void				SetService(shared_ptr<Service> service) { _service = service; }
 
 public:
-						/* Á¤º¸ °ü·Ã */
+						/* ì •ë³´ ê´€ë ¨ */
 	void				SetNetAddress(NetAddress address) { _netAddress = address; }
 	NetAddress			GetAddress() { return _netAddress; }
 	SOCKET				GetSocket() { return _socket; }
@@ -38,12 +38,12 @@ public:
 	SessionRef			GetSessionRef() { return static_pointer_cast<Session>(shared_from_this()); }
 
 private:
-						/* ÀÎÅÍÆäÀÌ½º ±¸Çö */
+						/* ì¸í„°í˜ì´ìŠ¤ êµ¬í˜„ */
 	virtual HANDLE		GetHandle() override;
 	virtual void		Dispatch(class IocpEvent* iocpEvent, int32 numOfBytes = 0) override;
 
 private:
-						/* Àü¼Û °ü·Ã */
+						/* ì „ì†¡ ê´€ë ¨ */
 	bool				RegisterConnect();
 	bool				RegisterDisconnect();
 	void				RegisterRecv();
@@ -57,7 +57,7 @@ private:
 	void				HandleError(int32 errorCode);
 
 protected:
-						/* ÄÁÅÙÃ÷ ÄÚµå¿¡¼­ ÀçÁ¤ÀÇ */
+						/* ì»¨í…ì¸  ì½”ë“œì—ì„œ ì¬ì •ì˜ */
 	virtual void		OnConnected() { }
 	virtual int32		OnRecv(BYTE* buffer, int32 len) { return len; }
 	virtual void		OnSend(int32 len) { }
@@ -80,12 +80,12 @@ private:
 private:
 	USE_LOCK;
 
-	/* ¼ö½Å °ü·Ã */
+	/* ìˆ˜ì‹  ê´€ë ¨ */
 
-	/* ¼Û½Å °ü·Ã */
+	/* ì†¡ì‹  ê´€ë ¨ */
 
 private:
-						/* IocpEvent Àç»ç¿ë */
+						/* IocpEvent ì¬ì‚¬ìš© */
 	ConnectEvent		_connectEvent;
 	DisconnectEvent		_disconnectEvent;
 	RecvEvent			_recvEvent;
