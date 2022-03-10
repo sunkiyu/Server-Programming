55208738deff80fefa1327ba13fe3cef21b1d821
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-10 09:54:45 +0900

    Delete Memory Pool#1 directory

--- Memory Pool#1/DummyClient/DummyClient.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "pch.h"
-#include <iostream>
-
-int main()
-{
-	HelloWorld();
-}
--- Memory Pool#1/ServerCore/CoreGlobal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern class ThreadManager*		GThreadManager;
-extern class Memory*			GMemory;
-
-extern class DeadLockProfiler*	GDeadLockProfiler;
-
