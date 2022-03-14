172c069a7d1ecb74273652b1a8e760b6f464ee9e
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-03-14 14:44:38 +0900

    Create Allocator.h

---
+++ 메모리 관리/Stomp-Allocator/Allocator.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/*-------------------
+	BaseAllocator
+-------------------*/
+
+class BaseAllocator
+{
+public:
+	static void*	Alloc(int32 size);
+	static void		Release(void* ptr);
+};
+
+/*-------------------
+	StompAllocator
+-------------------*/
+
+class StompAllocator
+{
+	enum { PAGE_SIZE = 0x1000 };
+
+public:
+	static void*	Alloc(int32 size);
+	static void		Release(void* ptr);
+};
